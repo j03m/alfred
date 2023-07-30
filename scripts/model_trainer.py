b953c5192187f6a005ca5640b61aba80bfd37591
jmordetsky@bloomberg.net <jmordetsky@bloomberg.net>
2023-07-30 08:13:39 -0400

    WIP - Trying out DummyVecEnv

--- scripts/model_trainer.py
+++ scripts/model_trainer.py
@@ -3,43 +3,19 @@ import os.path
 import argparse
 import pandas as pd
 from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.vec_env import DummyVecEnv
 import yfinance as yf
 from datetime import datetime, timedelta
+from typing import Callable
 import warnings
 
 warnings.filterwarnings('ignore', category=RuntimeWarning)
-
 
 from machine_learning_finance import TraderEnv, get_or_create_model, RangeTrainingWindowUtil, TailTrainingWindowUtil
 
 # handles UserWarning: Evaluation environment is not wrapped with a ``Monitor`` wrapper.
 from stable_baselines3.common.monitor import Monitor
 
-
-def download_symbol(symbol):
-    ticker_obj = yf.download(tickers=symbol, interval="1d")
-    return pd.DataFrame(ticker_obj)
-
-
-def train_model(symbol, df, args):
-    if args.tail is not None:
-        training_window = TailTrainingWindowUtil(df, args.tail)
-    else:
-        training_window = RangeTrainingWindowUtil(df, args.start, args.end)
-
-    env = TraderEnv(symbol, training_window.test_df, training_window.full_hist_df)
-
-    env = Monitor(env)
-
-    model, model_path, save_path = get_or_create_model(args.model_name, env, args.tensorboard_log_path)
-
-    eval_callback = EvalCallback(env, best_model_save_path=model_path,
-                                 log_path=model_path, eval_freq=500)
-
-    print("Train the agent for N steps")
-    for i in range(0, args.learning_runs):
-        model.learn(total_timesteps=args.training_intervals, tb_log_name=f"{args.learning_run_prefix}_{symbol}_{i}",
-                    callback=eval_callback, reset_num_timesteps=True if i == 0 else False)
 
 
 def main():
@@ -51,6 +27,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--symbol', type=str, default=None)
     parser.add_argument('--bulk', type=str, default=None)
+    parser.add_argument('--eval', type=str, default=None)
     parser.add_argument('--data-path', type=str, default="./data/")
     parser.add_argument('--training-intervals', type=int, default=1000)
     parser.add_argument('--tensorboard-log-path', type=str, default="./tensorboard-logs")
@@ -63,7 +40,6 @@ def main():
     parser.add_argument('--tail', type=int, default=None)
     parser.add_argument('--file', action="store_true")
 
-
     args = parser.parse_args()
 
     if args.symbol is not None:
@@ -74,16 +50,25 @@ def main():
         train_model(args.symbol, df, args)
     elif args.bulk is not None:
         if os.path.isfile(args.bulk):
-            symbols_df = pd.read_csv(args.bulk)
-            for symbol in symbols_df["Symbols"].values:
-                data_df = read_symbol_file(args, symbol)
-
-                train_model(symbol, data_df, args)
+            # training vector env
+            train_env = symbols_to_vec_env(args.bulk, args)
+            if args.eval is not None:
+                # eval vector env
+                eval_env = symbols_to_vec_env(args.eval, args)
+            else:
+                eval_env = train_env
+            train_model(train_env, eval_env, args)
         else:
             print(f"Could not find file {args.bulk}.")
     else:
         print("--symbol or --bulk must be supplied!")
         return -1
+
+
+def symbols_to_vec_env(file:str, args):
+    symbols_df = pd.read_csv(file)
+    train_env = get_vector_env(symbols_df.values, args)
+    return train_env
 
 
 def read_symbol_file(args, symbol, fail_on_missing=False):
@@ -102,6 +87,49 @@ def read_symbol_file(args, symbol, fail_on_missing=False):
             raise pe
     return data_df
 
+
+def download_symbol(symbol):
+    ticker_obj = yf.download(tickers=symbol, interval="1d")
+    return pd.DataFrame(ticker_obj)
+
+
+def get_environment_factory(symbol: str, df: pd.DataFrame, args: any) -> Callable[[], TraderEnv]:
+    def generate_environment() -> TraderEnv:
+        if args.tail is not None:
+            training_window = TailTrainingWindowUtil(df, args.tail)
+        else:
+            training_window = RangeTrainingWindowUtil(df, args.start, args.end)
+
+        return TraderEnv(symbol, training_window.test_df, training_window.full_hist_df)
+
+    return generate_environment
+
+
+def generate_evaluation_vec_env(symbols: [str], args):
+    for symbol in symbols:
+        data_df = read_symbol_file(args, symbol)
+        get_environment_factory(symbol, data_df, args)
+
+
+def get_vector_env(symbols: [str], args: any) -> DummyVecEnv:
+    environments = []
+    for symbol in symbols:
+        data_df = read_symbol_file(args, symbol)
+        environments.append(get_environment_factory(symbol, data_df, args))
+    return DummyVecEnv(environments)
+
+
+def train_model(train_env: DummyVecEnv, eval_env: DummyVecEnv, args: any):
+    # env = Monitor(env)
+
+    model, model_path, save_path = get_or_create_model(args.model_name, train_env, args.tensorboard_log_path)
+
+    eval_callback = EvalCallback(eval_env, best_model_save_path=model_path,
+                                 log_path=model_path, eval_freq=500)
+
+    print("Train the agent for N steps")
+    model.learn(total_timesteps=args.training_intervals, tb_log_name=f"{args.learning_run_prefix}_{i}",
+                callback=eval_callback, reset_num_timesteps=True)
 
 if __name__ == "__main__":
     main()
