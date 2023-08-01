60bfcfc4da83ec3a902e6897cf6b963013b7ae34
jmordetsky@bloomberg.net <jmordetsky@bloomberg.net>
2023-08-01 08:10:24 -0400

    re-cache and prune training list

--- src/machine_learning_finance/data_utils.py
+++ src/machine_learning_finance/data_utils.py
@@ -49,7 +49,10 @@ def download_ticker_list(ticker_list, output_dir="./data/", tail=-1, head=-1):
                 df = df.tail(tail)
             if head != -1:
                 df = df.head(head)
-            df.to_csv(os.path.join(output_dir, f"{ticker}.csv"))
+            if len(df) == 0:
+                bad_tickers.append(ticker)
+            else:
+                df.to_csv(os.path.join(output_dir, f"{ticker}.csv"))
         except Exception as e:
             print("Failed to download:", ticker, " with ", e)
             bad_tickers.append(ticker)
